9a0ccd78066350edcc7ffad7791d725b919a47df
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-02-11 12:52:46 -0600

    se añadio conexion a firebase

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './app/navigation/Navigation';
+import { firebaseApp } from './app/utils/firebase';
+import * as firebase from "firebase";
+
 
 export default function App() {
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user)
+    })
+  }, [])
+  
+
   return (
     <Navigation/>
   );
---
+++ app/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHfOvvBRKRrG9uq3GW5z235S2OgXm2rII",
+  authDomain: "appepa-7bef7.firebaseapp.com",
+  projectId: "appepa-7bef7",
+  storageBucket: "appepa-7bef7.appspot.com",
+  messagingSenderId: "793398088942",
+  appId: "1:793398088942:web:6f19b3877181845886cfff"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
