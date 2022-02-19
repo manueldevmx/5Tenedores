ebed90d590057909a17e5bbabf32f32c70ec43f3
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-02-18 23:44:28 -0600

    cambio de color bg login

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#FFFFFF',
+        backgroundColor: '#D8D5D5',
     },
 });
 
